4f6ad2d260d9ccca143244ef8a7b3e400986273b
Mirko Casoni <casoni.mirko@gmail.com>
2025-10-16 01:20:53 +0200

    fix: added variables seed and generations

--- Code/Program.cs
+++ Code/Program.cs
@@ -2,15 +2,17 @@
 
 int columns = 5;
 int rows = 3;
+int seed = 1234;
+int generations = 5;
 
-Grid grid = new Grid(columns, rows, 1234);
+Grid grid = new Grid(columns, rows, seed);
 
 Console.WriteLine("Grid initialized");
 grid.Print();
 
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < generations; i++)
 {
     grid.Next();
-    Console.WriteLine($"\nMove {i + 1}");
+    Console.WriteLine($"\nGeneration {i + 1}");
     grid.Print();
 }
